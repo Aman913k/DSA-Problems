f25068c87a4bfc3dcf035227c779ee7ddedf9188
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-04-08 14:24:31 +0530

    Time: 289 ms (83.01%), Space: 94.6 MB (33.47%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -9,14 +9,14 @@ public:
             if(total>=totalTrips) return true;
             i++;   
         }
-        return (total>=totalTrips)? true: false;  
+        return (total>=totalTrips)? true: false;   
     }
     
     
     
     long long minimumTime(vector<int>& time, int totalTrips) {
-        long long beg=1, end=1e14;
-        
+        long long beg=1, end=*min_element(time.begin(), time.end())*(long)totalTrips;
+          
         while(beg<=end){
             long long mid=beg+(end-beg)/2;
             if(check(time, totalTrips, mid)) end=mid-1;
