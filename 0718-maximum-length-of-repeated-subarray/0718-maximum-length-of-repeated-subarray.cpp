4a75bfa742e619f18cb5ace2ecf77d8295ee8752
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-04-28 00:17:42 +0530

    Time: 256 ms (56.45%), Space: 85.5 MB (51.90%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -9,8 +9,10 @@ public:
         for(int i=0; i<=n; i++){
             for(int j=0; j<=m; j++){
                 if(i==0||j==0) dp[i][j]=0; 
-                else if(nums1[i-1]==nums2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                ans=max(ans, dp[i][j]);
+                else if(nums1[i-1]==nums2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    ans=max(ans, dp[i][j]);
+                }
             }  
         }
           
