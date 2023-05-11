9fa75a3a7e4c5dd2708ad8a3653dc3b9f09aa9c9
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-05-11 12:46:04 +0530

    Time: 31 ms (27.91%), Space: 13 MB (63.96%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int helper(vector<int>& nums1, vector<int>& nums2, int indx1, int indx2, vector<vector<int>>& dp){
-        if(indx2>=nums2.size()||indx1>=nums1.size()) return 0;
-        if(dp[indx1][indx2]!=-1) return dp[indx1][indx2]; 
-         
-        int pick=0,nonpick=0;
-        if(nums1[indx1]==nums2[indx2]) pick=1+helper(nums1, nums2, indx1+1, indx2+1, dp);
+    int helper(vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& dp, int indx1, int indx2){
+        int n=nums1.size(), m=nums2.size();
+        if(indx1>=n || indx2>=m) return 0;
         
-        else
-        nonpick=max(helper(nums1, nums2, indx1+1, indx2, dp), helper(nums1, nums2, indx1, indx2+1, dp));  
-        return dp[indx1][indx2]=pick+nonpick; 
-    }  
+        if(dp[indx1][indx2]!=-1) return dp[indx1][indx2];
+        
+        if(nums1[indx1]==nums2[indx2]) return dp[indx1][indx2]=1+helper(nums1,nums2,dp,indx1+1,indx2+1);
+        
+        return dp[indx1][indx2]=max(helper(nums1,nums2,dp,indx1+1,indx2), helper(nums1,nums2,dp,indx1,indx2+1)); 
+    }
     
     
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        int indx1=0, indx2=0;
-        vector<vector<int>> dp(nums1.size(), vector<int> (nums2.size(), -1));
+        int n=nums1.size(), m=nums2.size();
         
-        return helper(nums1, nums2, indx1, indx2, dp); 
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return helper(nums1, nums2, dp, 0, 0);     
     }
 };
