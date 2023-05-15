8346efc46e014114f34d3e9d7b6afa9da503bcb2
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-05-16 02:26:45 +0530

    Time: 296 ms (20.43%), Space: 110.1 MB (54.74%) - LeetHub

--- 2611-mice-and-cheese/2611-mice-and-cheese.cpp
+++ 2611-mice-and-cheese/2611-mice-and-cheese.cpp
@@ -4,24 +4,19 @@ public:
         int n=reward1.size(); 
         
         priority_queue<pair<int,int>> pq;
+        int ans=0;
         
         for(int i=0; i<n; i++){
-            pq.push({reward1[i]-reward2[i], i});  
+            pq.push({reward1[i]-reward2[i], i});
+            ans+=reward2[i];
         }
-        
-        vector<int> indx;
-        
-        int ans=0;  
+         
         for(int i=0; i<k; i++){
-            indx.push_back(pq.top().second);   
+            ans+=reward1[pq.top().second];
+            ans-=reward2[pq.top().second];
             pq.pop();
-        }
-               
-        for(auto i: reward2) ans+=i;
-        for(int i=0; i<indx.size(); i++){
-            ans-=reward2[indx[i]];
-            ans+=reward1[indx[i]];
-        }
+        }         
+    
         return ans;            
     }
 };
