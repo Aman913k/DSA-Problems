bd32350af7634178714e79d46e82f31e67dfd260
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-06-11 21:27:10 +0530

    Time: 7 ms (100.00%), Space: 6.2 MB (100.00%) - LeetHub

--- 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.cpp
+++ 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int longestSemiRepetitiveSubstring(string s) {
-        int l=0,r=0;
-        int count=0;
+        int l=0,r=1;
+        int cntPair=0;
         int ans=1;
+        
         int n=s.size(); 
-        while(r<n-1){
-            r++;
-            if(s[r]==s[r-1]) count++;  
-            while(count>=2){
-                l++;
-                if(s[l]==s[l-1]) count--;
+        while(r<n){
+            if(s[r]==s[r-1]) cntPair++;
+            
+            while(cntPair>=2){
+                if(s[l]==s[l+1]) cntPair--;
+                l++;  
             }
+            
             ans=max(ans,r-l+1);
+            r++;
         }
         return ans;
     }
