f730dedf491bfbf7c637789b40400e956429b409
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-06-24 13:05:14 +0530

    Time: 46 ms (5.17%), Space: 23.3 MB (15.02%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -6,49 +6,15 @@
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
- */     
+ */
 
-class Solution {     
-public:      
-    void helper(TreeNode* root, TreeNode* parent, TreeNode* x,  vector<int>& v){
-        if(root==NULL) return;
-        
-        if(x->val<root->val){
-            v.push_back(root->val);
-            helper(root->left, root, x, v);
-        }
-        
-        else if(x->val>root->val){
-            v.push_back(root->val);
-            helper(root->right, root, x, v);   
-        }
-        
-        else{
-            v.push_back(root->val);;
-            return;
-        }
-        
-    }
-    
-    
-    
+class Solution {
+public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<int> vp, vq;
-        helper(root, NULL, p, vp);
-        helper(root, NULL, q, vq);
+        if(p->val<root->val && q->val<root->val) return lowestCommonAncestor(root->left, p, q);
+        else if(p->val>root->val && q->val>root->val) return lowestCommonAncestor(root->right, p, q);
         
-        int minIndx=min(vp.size(), vq.size());
+        return root;
         
-        int mini=0;
-        for(int i=minIndx-1; i>=0; i--){
-            if(vp[i]==vq[i]){
-                mini=vp[i];
-                break;
-            }
-        }         
-    
-        
-        TreeNode* node=new TreeNode(mini);
-        return node;     
     }
 };
