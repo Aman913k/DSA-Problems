83f00b2125bce059baa5bcc875d422d8b3aa2181
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-07-07 12:16:57 +0530

    Time: 27 ms (69.05%), Space: 10 MB (95.61%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        int n=answerKey.size();
-        int cntT=0, cntF=0;
-        int l=0, r=0;
-        int maxi=INT_MIN;
+        int n=answerKey.size(), cntT=0, cntF=0;
+        int l=0, r=0, maxi=INT_MIN;
         
         while(r<n){
             if(answerKey[r]=='T') cntT++;
             else cntF++;
             
             while(cntT>k && cntF>k){
-                maxi=max(maxi, r-l);
                 if(answerKey[l]=='T') cntT--;
                 else cntF--;
                 l++;
             }
+            
             maxi=max(maxi, r-l+1);
             r++;
         }
         
-        if(maxi==INT_MIN) return n; 
         return maxi;  
     }
 };
