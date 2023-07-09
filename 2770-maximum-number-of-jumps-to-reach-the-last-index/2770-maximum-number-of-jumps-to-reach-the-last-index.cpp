da4f5681d5decdc59bd9f74ffa2172242edca13b
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-07-09 12:44:17 +0530

    Time: 1029 ms (8.33%), Space: 65.7 MB (16.67%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -4,6 +4,7 @@ public:
     
     int helper(vector<int>& nums, int target,int prev, int indx){
         if(nums.size()==1) return 1;
+        if(indx>=nums.size()) return -1e9;
 
       
         if(indx==nums.size()-1) {
@@ -18,7 +19,9 @@ public:
         int pick=-1e9, nonpick=-1e9;  
         if(abs(nums[prev]-nums[indx])<=target) pick=1+helper(nums, target, indx, indx+1);
         nonpick=0+helper(nums, target, prev, indx+1);
-        
+         
+        //cout<<pick<<" "<<nonpick<<" "<<indx<<" "<<prev<<"   ";
+        //cout<<max(pick, nonpick)<<" ";
         return dp[indx][prev]=max(pick, nonpick); 
     }
     
@@ -27,6 +30,7 @@ public:
         memset(dp, -1, sizeof(dp));
         
       int ans=helper(nums, target, 0, 1);
+        cout<<endl;
         
         if(ans<=0) return -1;
         return ans;
