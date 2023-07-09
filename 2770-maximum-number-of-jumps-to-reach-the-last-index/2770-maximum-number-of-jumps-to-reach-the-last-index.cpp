a7ebdcddf6d81d1e7b6d04b255ed1f807930c4e6
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-07-09 11:40:20 +0530

    Time: 1043 ms (8.33%), Space: 65.8 MB (16.67%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -4,19 +4,22 @@ public:
     
     int helper(vector<int>& nums, int target,int prev, int indx){
         if(nums.size()==1) return 1;
+
       
         if(indx==nums.size()-1) {
             if(abs(nums[prev]-nums[indx])<=target) return 1;
             return -1e9;
         }
         
+       
+        
         if(dp[indx][prev]!=-1) return dp[indx][prev];
         
-        int ans=INT_MIN;  
-        if(abs(nums[prev]-nums[indx])<=target) ans=max(ans,1+helper(nums, target, indx, indx+1));
-        ans=max(ans,helper(nums, target, prev, indx+1));
+        int ans=INT_MIN, pick=INT_MIN, nonpick=INT_MIN;  
+        if(abs(nums[prev]-nums[indx])<=target) pick=1+helper(nums, target, indx, indx+1);
+        nonpick=0+helper(nums, target, prev, indx+1);
         
-        return dp[indx][prev]=ans; 
+        return dp[indx][prev]=max(pick, nonpick); 
     }
     
     int maximumJumps(vector<int>& nums, int target) {
