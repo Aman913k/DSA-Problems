8b8c5ca36e8a7b167b9fdee8aa289a46a1f458cf
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-07-27 01:35:44 +0530

    Time: 104 ms (29.40%), Space: 31 MB (54.23%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -26,16 +26,13 @@ public:
         
         int beg=*max_element(weights.begin(), weights.end()), end=totSum;   
         
-        int ans;  
+        
         while(beg<=end){
             int mid=beg+(end-beg)/2;    
             
-            if(psblMinWeight(weights, days, mid)){
-                ans=mid;
-                end=mid-1;
-            }
+            if(psblMinWeight(weights, days, mid)) end=mid-1;
             else beg=mid+1;
         }
-        return ans; 
+        return beg; 
     }
 };
