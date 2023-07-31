4f4c44e9a984f3819a9e17c8de32801661f75995
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-08-01 02:41:19 +0530

    Time: 8 ms (98.07%), Space: 13.3 MB (34.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -22,8 +22,10 @@ public:
         
         for(int i=n-1; i>0; i--){
             for(int j=m-1; j>0; j--){
-                if(matrix[i][0]==0) matrix[i][j]=0;
-                if(matrix[0][j]==0) matrix[i][j]=0;
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0) matrix[i][j]=0;
+                    if(matrix[0][j]==0) matrix[i][j]=0;
+                }
             }
         }
         
