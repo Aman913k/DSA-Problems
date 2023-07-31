ef537bf36852cf1dbe602a8d95a09aeb2beb5fba
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-07-31 20:49:22 +0530

    Time: 25 ms (89.55%), Space: 10.3 MB (84.31%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,35 +1,33 @@
 int dp[1001][1001];
 
-
 int helper(string& s1, string& s2, int n, int m, int indx1, int indx2){
     if(indx1>=n && indx2>=m) return 0;
     
-    if(indx1==n){
+    if(indx1>=n && indx2<m){
         int sum=0;
         for(int i=indx2; i<m; i++) sum+=s2[i]-0;
-        return sum;     
+        return sum;   
     }
     
-    if(indx2==m){
+    if(indx1<n && indx2>=m){
         int sum=0;
         for(int i=indx1; i<n; i++) sum+=s1[i]-0;
         return sum;
     }
-
     
     if(dp[indx1][indx2]!=-1) return dp[indx1][indx2];   
     
     
-    int pick=INT_MAX, nonpick=INT_MAX; 
-    
+    int pick=INT_MAX, nonpick=INT_MAX, np1=0, np2=0;          
     if(s1[indx1]==s2[indx2]){
         pick=0+helper(s1, s2, n, m , indx1+1, indx2+1);
     }
-   else nonpick=min((s1[indx1]-0)+helper(s1, s2, n, m , indx1+1, indx2), (s2[indx2]-0)+helper(s1, s2, n, m, indx1, indx2+1));
+    
+    else nonpick=min((s1[indx1]-0)+helper(s1, s2, n, m , indx1+1, indx2), (s2[indx2]-0)+helper(s1, s2, n, m, indx1, indx2+1));
     
     
     
-    return dp[indx1][indx2]=min(pick, nonpick);     
+    return dp[indx1][indx2]=min(pick,nonpick);   
     
 }
     
