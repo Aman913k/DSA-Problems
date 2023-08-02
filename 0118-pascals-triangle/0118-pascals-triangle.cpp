fe503d577408dac68d6fb402d682db3cac691899
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-08-03 01:19:12 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (34.68%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -5,7 +5,6 @@ public:
         
         for(int i=0; i<numRows; i++){
             vector<int> v(i+1, 1);
-            if(i<2) ans[i]=v;
             
             for(int j=1; j<i; j++){
                 v[j]=ans[i-1][j-1]+ans[i-1][j];
