6376cd6dd880f748c8311e84ffd845bf6a34e066
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-08-03 21:34:55 +0530

    Time: 3 ms (40.70%), Space: 6.7 MB (71.86%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,17 +1,26 @@
+void printN(int n, vector<vector<int>>& ans){
+    vector<int> v(n, 1);  
+    long long p=n-1;
+    long long k=n-1;
+    
+    for(int i=1; i<n-1; i++){ 
+        v[i]=p/i;
+        k-=1;
+        p=v[i]*(k);
+    }
+    ans.push_back(v); 
+}          
+
+
+
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans(numRows);
+        vector<vector<int>> ans;
         
-        for(int i=0; i<numRows; i++){
-            vector<int> v(i+1, 1);
-            
-            for(int j=1; j<i; j++){
-                v[j]=ans[i-1][j-1]+ans[i-1][j];
-            }
-            
-            ans[i]=v;
+        for(int i=1; i<=numRows; i++){
+            printN(i, ans); 
         }
-        return ans;
+        return ans; 
     }
 };
