6083e268fd3f5c8bb4c33eddee78ac68f30fa547
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-08-04 23:12:13 +0530

    Time: 36 ms (93.44%), Space: 7.4 MB (86.57%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,23 +1,24 @@
-class Solution {
-public:
-    int helper(string& s, string& s1, int indx1, int indx2, vector<vector<int>>& dp, int n){
-        if(indx1==n || indx2==n) return 0;
-        if(dp[indx1][indx2]!=-1) return dp[indx1][indx2]; 
-        
+int dp[501][501];
 
-        if(s[indx1]==s1[indx2]) return dp[indx1][indx2]=1+helper(s, s1, indx1+1, indx2+1, dp, n);
-        return dp[indx1][indx2]=max(helper(s, s1, indx1+1, indx2, dp, n), helper(s, s1, indx1, indx2+1, dp, n));
-      
-    }
+
+int helper(string& s, string& t, int indx1, int indx2, int n){
+    if(indx1>=n || indx2>=n) return 0;
+    if(dp[indx1][indx2]!=-1) return dp[indx1][indx2];
     
+    if(s[indx1]==t[indx2]) return dp[indx1][indx2]=1+helper(s, t, indx1+1, indx2+1, n);
     
-    
-    int minInsertions(string s) {  
-        string s1=s; 
-        reverse(s1.begin(), s1.end());
-        int n=s.size(); 
-        vector<vector<int>> dp(n, vector<int>(n,-1));  
+    return dp[indx1][indx2]=max(helper(s, t, indx1+1, indx2, n), helper(s, t, indx1, indx2+1, n));
+}
+
+class Solution {     
+public:
+    int minInsertions(string s) {
+        int n=s.size();
         
-        return s.size()-(helper(s, s1, 0, 0, dp, n));
+        string t=s;
+        reverse(t.begin(), t.end());
+        
+        memset(dp, -1, sizeof(dp)); 
+        return n-helper(s, t, 0, 0, n);
     }
 };
