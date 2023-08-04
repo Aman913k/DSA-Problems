e658710565818d9357e7fc8a366d9f9ca4367eb5
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-08-04 22:05:25 +0530

    Time: 477 ms (5.22%), Space: 73.1 MB (10.45%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,12 +1,12 @@
 int helper(string& s1, string& s2, int indx1, int indx2, int n, vector<vector<int>>& dp){
-    if(indx1>=n || indx2>=n) return 0;
+    if(indx1<0 || indx2<0) return 0;
     if(dp[indx1][indx2]!=-1) return dp[indx1][indx2];
     
     int pick=0, nonpick=0;
     
-    if(s1[indx1]==s2[indx2]) pick=1+helper(s1, s2, indx1+1, indx2+1, n, dp);
+    if(s1[indx1]==s2[indx2]) pick=1+helper(s1, s2, indx1-1, indx2-1, n, dp);
     
-    nonpick=0+max(helper(s1, s2, indx1+1, indx2, n, dp), helper(s1, s2, indx1, indx2+1, n, dp));
+    nonpick=0+max(helper(s1, s2, indx1-1, indx2, n, dp), helper(s1, s2, indx1, indx2-1, n, dp));
     
     return dp[indx1][indx2]=max(pick, nonpick);
     
@@ -26,6 +26,6 @@ public:
         
         vector<vector<int>> dp(n, vector<int>(n, -1));
         
-        return helper(s, ss, 0, 0, n, dp);          
+        return helper(s, ss, n-1, n-1, n, dp);          
     }
 };
