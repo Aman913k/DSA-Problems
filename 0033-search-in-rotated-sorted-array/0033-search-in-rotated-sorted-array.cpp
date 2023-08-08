c2e5acc123a5a9289b91ebcd3ed6e90f69932ffc
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-08-08 17:08:02 +0530

    Time: 2 ms (77.39%), Space: 11.2 MB (13.70%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,8 +1,12 @@
 int findBreak(vector<int>& nums){
-    for(int i=1; i<nums.size(); i++){
-        if(nums[i]<nums[i-1]) return i-1;
+    int l=0, r=nums.size()-1;
+    
+    while(l<r){
+        int mid=(l+r)/2;
+        if(nums[mid]>nums[r]) l=mid+1;
+        else r=mid;
     }
-    return -1;
+    return l-1;
 }
 
 class Solution {
