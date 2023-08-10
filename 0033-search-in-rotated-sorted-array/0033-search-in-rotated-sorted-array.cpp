c09e6692ee2af8f16c5bf35045febc7624822468
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-08-11 00:22:06 +0530

    Time: 0 ms (100.00%), Space: 11 MB (50.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,27 +5,22 @@ int findBreak(vector<int>& nums){
         int mid=(l+r)/2;
         if(nums[mid]>nums[r]) l=mid+1;
         else r=mid;
-    }
+    }   
     return l-1;
 }
+
 
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-          
-        if(n==1){
-            if(nums[0]!=target) return -1;
-            return 0;  
-        }
+        int n=nums.size();   
         
         int indx=findBreak(nums);
-        
         int l=0, r=n-1;
-
-        if(indx!=-1){    // Rotation hai   
-            if(target<=nums[indx] && target>=nums[0]){
-                l=0;
+        
+        if(indx!=-1){    // Array is Rotated   
+            if(target<=nums[indx] && target>=nums[0]){            
+                l=0; 
                 r=indx;
             }
             else{
@@ -34,21 +29,13 @@ public:
             }
         }
         
-        
         while(l<=r){
             int mid=(l+r)/2;
             if(nums[mid]==target) return mid;
             else if(nums[mid]<target) l=mid+1;
             else r=mid-1;
-        }
+        } 
         
-        
-        
-        
-        //cout<<indx;
-        // if(nums[l]==target) return l;
-        // else if(nums[n/2]==target) return n/2;  
-         return -1;
-         
+        return -1;   
     }
 };
