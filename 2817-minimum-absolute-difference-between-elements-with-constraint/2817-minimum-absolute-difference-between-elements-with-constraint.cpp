cb49f6d68e79ebb6ff68f578af7309f8116ae17f
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-08-13 16:53:39 +0530

    Time: 254 ms (100.00%), Space: 118 MB (20.00%) - LeetHub

---
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minAbsoluteDifference(vector<int>& nums, int x) {
+        int n=nums.size();
+        
+      
+        set<int> st;
+        int mini=INT_MAX;
+        for(int i=x; i<n; i++){
+            st.insert(nums[i-x]); 
+            auto it=st.upper_bound(nums[i]);
+            auto it1=st.upper_bound(nums[i]+1);
+            if(it!=st.end()) mini=min(mini, abs(nums[i]-*it));
+            if(it!=st.begin()) mini=min(mini, abs(nums[i]-*prev(it)));
+            
+        }
+        
+        return mini;  
+        
+    }   
+        
+};
