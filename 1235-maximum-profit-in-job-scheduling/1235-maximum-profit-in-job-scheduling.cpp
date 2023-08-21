88c321ab07bd3fed5174eb9ae3ade6deff16e26d
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-08-21 19:46:20 +0530

    Time: 94 ms (93.32%), Space: 52 MB (85.46%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,12 +1,12 @@
 int dp[100001];
-int helper(vector<pair<pair<int,int>,int>>& v, vector<int>& t, int indx){
-    if(indx>=v.size()) return 0;
+int helper(vector<pair<pair<int,int>,int>>& jobs, vector<int>& start, int indx){
+    if(indx>=jobs.size()) return 0;
     if(dp[indx]!=-1) return dp[indx]; 
     
     int pick=0, nonpick=0;
-    auto it=lower_bound(t.begin(), t.end(), v[indx].first.second)-t.begin();    
-    pick=v[indx].second+helper(v, t, it);
-    nonpick=helper(v, t, indx+1);
+    auto it=lower_bound(start.begin(), start.end(), jobs[indx].first.second)-start.begin();  
+    pick=jobs[indx].second+helper(jobs, start, it);
+    nonpick=helper(jobs, start, indx+1);
     
     return dp[indx]=max(pick, nonpick);   
     
@@ -16,18 +16,16 @@ int helper(vector<pair<pair<int,int>,int>>& v, vector<int>& t, int indx){
 class Solution {
 public:
     int jobScheduling(vector<int>& Stime, vector<int>& Etime, vector<int>& profit) {
-        vector<pair<pair<int,int>,int>> v(Stime.size());
+        vector<pair<pair<int,int>,int>> jobs;
         memset(dp,-1,sizeof (dp));
         for(int i=0; i<Stime.size(); i++){
-            v[i].first.first=Stime[i];
-            v[i].first.second=Etime[i];
-            v[i].second=profit[i];    
+            jobs.push_back({{Stime[i], Etime[i]}, profit[i]});      
         }
         
-        vector<int> t;
-        sort(v.begin(), v.end());
-        for(auto it: v) t.push_back(it.first.first);
+        vector<int> start;
+        sort(jobs.begin(), jobs.end());
+        for(auto it: jobs) start.push_back(it.first.first);
         
-        return helper(v, t, 0);
+        return helper(jobs, start, 0);
     }
 };
