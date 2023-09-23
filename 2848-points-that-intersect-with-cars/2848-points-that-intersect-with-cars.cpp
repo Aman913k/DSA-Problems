3e05842993efcdd76d914d351999d3b33ddc5680
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2023-09-24 02:30:03 +0530

    Time: 29 ms (14.10%), Space: 26.7 MB (12.73%) - LeetHub

---
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfPoints(vector<vector<int>>& nums) {
+        set<int>st;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=nums[i][0]; j<=nums[i][1]; j++){
+                st.insert(j);
+            }
+        }
+        return st.size();
+    }
+};
