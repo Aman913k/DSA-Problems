c0e891940bb05fd5b17c1eeb537366f02719b309
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2024-01-01 00:56:24 +0530

    Time: 3 ms (47.38%), Space: 6.7 MB (75.31%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
         int maxi=-1; 
-        int n=s.size(); 
-        for(int i=0; i<n; i++){
-            char ch=s[i];
-            for(int j=n-1; j>=i; j--){
-                if(s[j]==ch){
-                    maxi=max(maxi, j-i-1);
-                    break;
-                } 
-            }
+        int n=s.size();
+        vector<int> last(26);
+        for(int i=0; i<n; i++) last[s[i]-'a']=i;
+        
+        for(int i=0; i<n; i++){  
+            if(i!=last[s[i]-'a']) maxi=max(maxi, (last[s[i]-'a']-i-1));
         }
+        
         return maxi;
     }
 };
