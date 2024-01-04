7d9e67d5cccad3d8251948fa7f9d5049e5ce4661
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2024-01-04 23:13:16 +0530

    Time: 20 ms (33.64%), Space: 20.6 MB (13.32%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    
     int countSubstrings(string s) {
         int n=s.size();
-        vector<vector<int>> dp(n, vector<int> (n,0));
-        int sum=0;
+        vector<vector<int>> dp(n+1, vector<int>(n+1)); 
+        int tot=0;
         
-        for(int diff=0; diff<n; diff++){
-            for(int indx1=0, indx2=diff+indx1; indx2<n; indx1++, indx2++){
-                if(indx1==indx2) dp[indx1][indx2]=1;
-                
-                else if(diff==1){
-                    if(s[indx1]==s[indx2]) dp[indx1][indx2]=1;
-                    else dp[indx1][indx2]=0;
-                }
-                
+        //for(int i=0; i<n; i++) dp[i][i]=1; 
+        
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; j<n; j++){
+                if(i==j) dp[i][j]=1;
+                else if((j-i)==1 && s[i]==s[j]) dp[i][j]=1;
                 else{
-                    if(s[indx1]==s[indx2] && dp[indx1+1][indx2-1]) dp[indx1][indx2]=1;
-                    else dp[indx1][indx2]=0;
+                    if(s[i]==s[j] && dp[i+1][j-1]) dp[i][j]=1;  
+                    else dp[i][j]=0;
                 }
-                
-                sum+=dp[indx1][indx2];
+                tot+=dp[i][j]; 
             }
         }
-        return sum;  
+        
+        return tot; 
     }
 };
