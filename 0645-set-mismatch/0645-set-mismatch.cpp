61a72c67087eb246bf6cc3ee05a1dd73376ee879
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2024-01-22 22:45:42 +0530

    Time: 24 ms (69.83%), Space: 23.6 MB (37.48%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,18 +1,26 @@
+#define ll long long
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> ans;
-        unordered_map<int,int> mp;
-        for(auto i: nums) mp[i]++;
-        int miss=0;
+        ll n=nums.size();
         
-        for(int i=1; i<=n; i++){
-            if(mp[i]==2) ans.push_back(i);
-            if(mp[i]==0) miss=i; 
+        ll S=0, S2=0;  
+        ll Sn=n*(n+1)/2;
+        ll S2n=n*(n+1)*(2*n+1)/6;
+        
+        for(int i=0; i<n; i++){
+            S+=nums[i];
+            S2+=(nums[i]*nums[i]); 
         }
-      
-        ans.push_back(miss);   
-        return ans;   
+        
+        ll a=S-Sn;
+        ll b=S2-S2n;  
+        
+        ll c=b/a;
+        ll repeating=(a+c)/2;
+        ll missing=repeating-a;    
+        
+        return{(int)repeating, (int)missing}; 
+        
     }
 };
