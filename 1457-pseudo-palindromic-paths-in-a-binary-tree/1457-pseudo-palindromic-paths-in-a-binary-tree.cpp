3c0233c5d815c35f1cb157709c453dc71b76052f
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2024-01-25 13:53:52 +0530

    Time: 270 ms (63.14%), Space: 177.6 MB (98.86%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -18,7 +18,7 @@ public:
     map<int, int> mp;
     int ans=0;
       
-    void helper(TreeNode* root, map<int,int>& mp){
+    void helper(TreeNode* root){
         mp[root->val]++;
         if(!root->left && !root->right){
             int x=0;
@@ -27,15 +27,15 @@ public:
             mp[root->val]--;
             return;
         }
-        if(root->left) helper(root->left, mp);
-        if(root->right) helper(root->right, mp);
+        if(root->left) helper(root->left);
+        if(root->right) helper(root->right);
         mp[root->val]--;
     }
     
     
     int pseudoPalindromicPaths (TreeNode* root) {
 
-        helper(root, mp);
+        helper(root);
         return ans;
         
     }
