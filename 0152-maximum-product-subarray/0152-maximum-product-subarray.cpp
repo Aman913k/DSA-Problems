e365956beb87be87514af5539d02fb1e16c33831
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2024-03-15 13:58:15 +0530

    Time: 7 ms (37.99%), Space: 16.2 MB (74.99%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-        int n=nums.size();
-        int maxi=0, neg=0, prefProd=1;     
+
+   int maxProduct(vector<int>& nums) {  
+    int n=nums.size();  
+	int maxi=INT_MIN, neg=0, prefProd=1;     
+    
+    for(int i=0; i<n; i++){       
+        prefProd*=nums[i];
+        maxi=max(maxi, prefProd);  
         
-        if(n==1) return nums[0];  
-        
-        for(int i=0; i<n; i++){       
-            prefProd*=nums[i];
-            
-            if(prefProd==0){  
-                prefProd=1;
-                neg=0; 
-            }
-            else if(prefProd<0){
-                if(neg==0) neg=prefProd;
-                else maxi=max(maxi, prefProd/neg);
-            }
-            else maxi=max(maxi, prefProd); 
+        if(prefProd==0){              // Whenever we encouner prefix product==0 just restart 
+            prefProd=1;
+            neg=0; 
         }
-        
-        return maxi; 
+        else if(prefProd<0){
+            if(neg==0) neg=prefProd;
+            else maxi=max(maxi, prefProd/neg);
+        }
     }
+    
+    return maxi; 
+  }
 };
