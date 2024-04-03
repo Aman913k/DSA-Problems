0198b9d79a929fc85e87a297619f4267a60c7f91
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2024-04-04 00:05:59 +0530

    Time: 1006 ms (30.62%), Space: 11.7 MB (49.58%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -33,7 +33,7 @@ public:
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){  
-                if(board[i][j]==word[0] && dfs(board, word, vis, i, j, 0)) return true;
+                if(dfs(board, word, vis, i, j, 0)) return true;
             }  
         }
         
