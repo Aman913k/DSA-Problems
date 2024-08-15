40dc749771b9f3c0bd61dc8d5ac0b317e10cdaad
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2024-08-15 13:00:27 +0530

    Time: 86 ms (7.76%), Space: 87.3 MB (5.06%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n=bills.size();
+        unordered_map<int,int> mp;
+        
+        for(int i=0; i<n; i++){
+            if(bills[i]==5) mp[bills[i]]++;
+            else if(bills[i]==10){
+                if(mp.find(5)==mp.end()) return false;
+                mp[5]--;
+                mp[10]++;
+            }
+            else if(bills[i]==20){
+                if(mp.find(10)!=mp.end() && mp[10]>=1 && mp.find(5)!=mp.end() && mp[5]>=1){
+                    mp[10]--;
+                    mp[5]--; 
+                }
+                else if(mp.find(5)!=mp.end() && mp[5]>=3){
+                    mp[5]-=3;
+                }
+                else return false; 
+            }
+        }
+        return true;  
+    }
+};
