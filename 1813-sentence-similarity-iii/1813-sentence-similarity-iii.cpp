7a5fa23f0b011c51a557acf7d6ee4831f6de88ee
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2024-10-07 20:01:36 +0530

    Time: 5 ms (8.72%), Space: 8.4 MB (76.15%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool areSentencesSimilar(string sent1, string sent2) {
+        vector<string> v1,v2; 
+        string s;
+        
+        for(int i=0; i<sent1.size(); i++){
+            if(sent1[i]==' '){
+                v1.push_back(s);
+                s="";
+                continue;
+            }
+            s+=sent1[i]; 
+        }
+        v1.push_back(s);
+        s=""; 
+        
+        for(int i=0; i<sent2.size(); i++){
+            if(sent2[i]==' '){
+                v2.push_back(s);
+                s="";
+                continue;
+            }
+            s+=sent2[i];  
+        }
+        v2.push_back(s);
+        
+        if(v1.size()<v2.size()) swap(v1, v2);   
+      
+        int i=0, n=v1.size(), m=v2.size(), j1=n-1, j2=m-1; 
+        while(i<m&&i<n && v1[i]==v2[i]) i++;
+        while(j1>=i && j2>=i && v1[j1]==v2[j2]){
+            cout<<"Fsiufuns"<<endl;
+            j1--;  
+            j2--; 
+        }
+       
+        if(j2<i) return true;
+        return false; 
+        
+    }
+};
