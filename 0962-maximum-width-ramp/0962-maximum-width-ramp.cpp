74c2f0cb7a3bf546834e140b6567aaafa9ff32e2
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2024-10-10 13:47:50 +0530

    Time: 68 ms (17.54%), Space: 47.3 MB (18.67%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.cpp
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.cpp
@@ -8,20 +8,18 @@ public:
         
         for(int i=1; i<n; i++) suff[i]=max(nums[n-i-1], suff[i-1]);
         int ans=INT_MIN; 
-        // for(auto i: suff) cout<<i<<" ";
-        // cout<<endl; 
+         
                    
         for(int i=0; i<n; i++){
             int tar=nums[i];  
+            int l=0, h=n-i-2; 
             
-            int l=0, h=n-i-2;  
-            // cout<<i<<" "<<l<<" "<<h<<endl; 
             while(l<=h){
                 int mid=l+(h-l)/2;   
                 if(suff[mid]>=tar) h=mid-1;
                 else l=mid+1;
             }
-            // cout<<h+1<<" "<<suff[h+1]<<endl; 
+        
             if(h+1>=0 && h+1<n && suff[h+1]>=tar) ans=max(ans, n-h-2-i);
         }
         return ans; 
