eea5b5939e05915253a2b70944f43428ffa800c6
Aman Kumar <95915282+Aman913k@users.noreply.github.com>
2024-10-12 13:15:27 +0530

    Time: 69 ms (45.65%), Space: 15.8 MB (16.33%) - LeetHub

---
+++ 3297-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3297-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -0,0 +1,42 @@
+#define ll long long
+class Solution {
+public:
+    long long validSubstringCount(string word1, string word2) {
+        map<char,int> mp,mp2;
+        vector<int> Given(26);
+        for(auto i: word2) Given[i-'a']++; 
+        // for(auto i: word2) mp2[i]++;
+        
+        ll cnt=0;  
+        ll l=0, r=0;
+        int n=word1.size();
+        // for(auto i: word2) mp1[i]++;
+        
+        ll ans=0;   
+        while(r<n){
+            mp[word1[r]]++;
+        
+            if(Given[word1[r]-'a']){
+                
+                if(mp[word1[r]]<=Given[word1[r]-'a']){
+                    cnt++;  
+        
+                }
+                
+            }
+            
+            while(cnt==word2.size()){
+                ans+=(n-r);    
+                if(Given[word1[l]-'a']){
+                    mp[word1[l]]--;
+                    if(mp[word1[l]]<Given[word1[l]-'a']) cnt--; 
+                }  
+                l++;
+            }
+            
+            r++;
+        }
+        
+        return ans;
+    }
+};
